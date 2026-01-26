ca1b4140d0ca313e9f3ec31be3969e8cfba444c7
dong <dho0647@naver.com>
2026-01-26 14:09:51 +0900

    feat: 자체 로그인 구현 추가

--- src/main/java/com/example/account/repository/auth/MemberRoleGroupRepository.java
+++ src/main/java/com/example/account/repository/auth/MemberRoleGroupRepository.java
@@ -1,7 +1,14 @@
 package com.example.account.repository.auth;
 
+import com.example.account.domain.entity.auth.Member;
 import com.example.account.domain.entity.auth.MemberRoleGroup;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemberRoleGroupRepository extends JpaRepository<MemberRoleGroup, Long> {
+
+    @EntityGraph(attributePaths = "roleGroup")
+    List<MemberRoleGroup> findByMember(Member member);
 }
--- src/main/java/com/example/account/service/CustomUserDetailsService.java
+++ src/main/java/com/example/account/service/CustomUserDetailsService.java
@@ -1,28 +1,44 @@
 package com.example.account.service;
 
+import com.example.account.domain.entity.auth.Member;
 import com.example.account.domain.entity.auth.MemberLocalCredential;
+import com.example.account.domain.entity.auth.MemberRoleGroup;
 import com.example.account.repository.auth.MemberLocalCredentialRepository;
-import com.example.account.repository.auth.MemberRepository;
+import com.example.account.repository.auth.MemberRoleGroupRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberLocalCredentialRepository localRepository;
+    private final MemberRoleGroupRepository roleGroupRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        MemberLocalCredential user = localRepository.findMemberLocalCredentialByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+        MemberLocalCredential user = localRepository
+                .findMemberLocalCredentialByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+        Member member = user.getMember();
+        List<MemberRoleGroup> roleGroups = roleGroupRepository.findByMember(member);
+        List<SimpleGrantedAuthority> authorities = roleGroups.stream()
+                .map(rg -> new SimpleGrantedAuthority("ROLE" + rg.getRoleGroup().getRole().name()))
+                .toList();
 
         return User.builder()
                 .username(user.getEmail())
                 .password(user.getPassword())
+                .authorities(authorities)
                 .build();
     }
 }
